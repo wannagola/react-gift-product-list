4609100d23af8fb3c0ef5aea763129aebc64d462
MINKI SUNG <162106492+wannagola@users.noreply.github.com>
2025-08-01 18:29:58 +0900

    refactor : review 반영/ 최종 수정

--- src/api/ranking.ts
+++ src/api/ranking.ts
@@ -6,17 +6,9 @@ export type TabValue = 'MANY_WISH' | 'MANY_RECEIVE' | 'MANY_WISH_RECEIVE';
 export interface Product {
   id: number;
   name: string;
-  price: {
-    basicPrice: number;
-    sellingPrice: number;
-    discountRate: number;
-  };
+  price: number;
   imageURL: string;
-  brandInfo: {
-    id: number;
-    name: string;
-    imageURL: string;
-  };
+  brandName: string;
 }
 
 export const fetchRanking = async (
